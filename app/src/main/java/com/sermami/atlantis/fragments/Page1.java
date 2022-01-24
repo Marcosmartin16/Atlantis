f92a6318af4a6cf3bd98b4cbcc2968365e8a9ab7
SergioTS2101 <sergiots2101@gmail.com>
2022-01-24 12:39:19 +0100

    Terminado splash y login

--- app/src/main/java/com/sermami/atlantis/fragments/Page1.java
+++ app/src/main/java/com/sermami/atlantis/fragments/Page1.java
@@ -61,6 +61,6 @@ public class Page1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_page1, container, false);
+        return inflater.inflate(R.layout.login_fragment, container, false);
     }
 }
