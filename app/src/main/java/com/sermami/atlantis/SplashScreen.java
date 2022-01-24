b438b3320884013779b3ac39e9d8acafa92bb46f
marcosmartin16 <sr_eme@outlook.es>
2022-01-24 11:42:29 +0100

    splash

--- app/src/main/java/com/sermami/atlantis/SplashScreen.java
+++ app/src/main/java/com/sermami/atlantis/SplashScreen.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,13 +19,29 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
 
+        getSupportActionBar().hide();
+
         openApp(true);
 
-        ImageView mFondo = findViewById(R.id.fondo);
+        ImageView pompa1 = findViewById(R.id.pompa1);
+        ImageView pompa2 = findViewById(R.id.pompa2);
+        ImageView pompa3 = findViewById(R.id.pompa3);
+        ImageView pompa4 = findViewById(R.id.pompa4);
+        ImageView pompa5 = findViewById(R.id.pompa5);
+        ImageView pompa6 = findViewById(R.id.pompa6);
+        ImageView pompa7 = findViewById(R.id.pompa7);
+        ImageView pompa8 = findViewById(R.id.pompa8);
 
-        Glide.with(this)
-                .load(R.drawable.fondo)
-                .into(mFondo);
+
+        Animation scale = AnimationUtils.loadAnimation(this,R.anim.scale);
+        pompa1.startAnimation(scale);
+        pompa2.startAnimation(scale);
+        pompa3.startAnimation(scale);
+        pompa4.startAnimation(scale);
+        pompa5.startAnimation(scale);
+        pompa6.startAnimation(scale);
+        pompa7.startAnimation(scale);
+        pompa8.startAnimation(scale);
     }
 
     public void openApp(boolean locationPermission){
@@ -38,6 +56,6 @@ public class SplashScreen extends AppCompatActivity {
 
                 startActivity(intent);
             }
-        }, 3000);
+        }, 80000);
     }
 }
