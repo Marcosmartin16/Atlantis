625ae59440ca383a79c1f98645d370e18ba5ef21
marcosmartin16 <sr_eme@outlook.es>
2022-01-31 12:51:49 +0100

    activity historia terminada

---
+++ app/src/main/java/com/sermami/atlantis/Seleccion_historia.java
@@ -0,0 +1,17 @@
+package com.sermami.atlantis;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Seleccion_historia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccion_historia);{
+
+
+        }
+    }
+}
