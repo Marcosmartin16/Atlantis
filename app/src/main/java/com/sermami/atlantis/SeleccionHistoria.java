ae034cff65a44b9fdd9e3a21f1a1702329f1c7d1
marcosmartin16 <sr_eme@outlook.es>
2022-02-01 08:31:46 +0100

    Funciona todo

--- app/src/main/java/com/sermami/atlantis/SeleccionHistoria.java
+++ app/src/main/java/com/sermami/atlantis/SeleccionHistoria.java
@@ -4,14 +4,15 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Seleccion_historia extends AppCompatActivity {
+public class SeleccionHistoria extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_seleccion_historia);{
+        setContentView(R.layout.activity_seleccion_historia);
+
+        getSupportActionBar().hide();
 
 
-        }
     }
 }
