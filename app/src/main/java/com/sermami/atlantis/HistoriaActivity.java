1d59e16f6e66ba2799321039bb646599b28cba88
SergioTS2101 <sergiots2101@gmail.com>
2022-02-01 11:53:42 +0100

    Modificando el activity_main

--- app/src/main/java/com/sermami/atlantis/HistoriaActivity.java
+++ app/src/main/java/com/sermami/atlantis/HistoriaActivity.java
@@ -4,11 +4,12 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SignupActvity extends AppCompatActivity {
+public class HistoriaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signup_activity);
+        setContentView(R.layout.activity_historia);
+
     }
 }
--- app/src/main/java/com/sermami/atlantis/MainActivity.java
+++ app/src/main/java/com/sermami/atlantis/MainActivity.java
@@ -7,12 +7,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    ConstraintLayout clp;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
 
 
     }
