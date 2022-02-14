2e65257474a929fb99d02bb57ad48a0d217ac5d1
SergioTS2101 <sergiots2101@gmail.com>
2022-02-14 13:59:16 +0100

    Condiciones y terminos

--- app/src/main/java/com/sermami/atlantis/fragments/LoginFragment.java
+++ app/src/main/java/com/sermami/atlantis/fragments/LoginFragment.java
@@ -15,52 +15,17 @@ import com.sermami.atlantis.R;
 
 public class LoginFragment extends Fragment {
 
-        // TODO: Rename parameter arguments, choose names that match
-        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-        private static final String ARG_PARAM1 = "param1";
-        private static final String ARG_PARAM2 = "param2";
 
-        Button entrar;
+    Button entrar;
 
-        // TODO: Rename and change types of parameters
-        private String mParam1;
-        private String mParam2;
 
-        public LoginFragment() {
-            // Required empty public constructor
-        }
+    public LoginFragment() {
+    }
 
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Page1.
-         */
-        // TODO: Rename and change types and number of parameters
-        public static LoginFragment newInstance(String param1, String param2) {
-            LoginFragment fragment = new LoginFragment();
-            Bundle args = new Bundle();
-            args.putString(ARG_PARAM1, param1);
-            args.putString(ARG_PARAM2, param2);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            if (getArguments() != null) {
-                mParam1 = getArguments().getString(ARG_PARAM1);
-                mParam2 = getArguments().getString(ARG_PARAM2);
-            }
-        }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View myView = inflater.inflate(R.layout.login_fragment, container, false);
 
         entrar = (Button) myView.findViewById(R.id.btn_entrar);
