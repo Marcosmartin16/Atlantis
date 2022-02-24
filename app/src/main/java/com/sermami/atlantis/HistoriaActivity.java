79f3be4b422c2a16e32993b6a1676397237c0903
miguemoliaga <miguel.mo1999@gmail.com>
2022-02-24 09:02:28 +0100

    SWITCH CASE FIXED

--- app/src/main/java/com/sermami/atlantis/HistoriaActivity.java
+++ app/src/main/java/com/sermami/atlantis/HistoriaActivity.java
@@ -16,7 +16,9 @@ import android.widget.TextView;
 import com.sermami.atlantis.databinding.ActivityScrollingBinding;
 
 public class HistoriaActivity extends AppCompatActivity {
-
+    private enum Animales{
+        ajolote, caballito, langosta, lobo, coral, anemona,pez, foca, tiburon, tortuga, ballena,narval;
+    }
     private ActivityScrollingBinding binding;
     String temaElegido;
     MainActivity ma;
@@ -40,88 +42,69 @@ public class HistoriaActivity extends AppCompatActivity {
         tv = (TextView) findViewById(R.id.tvHistoria);
 
         temaElegido = getIntent().getStringExtra("temaElegido");
-/*        switch (temaElegido){
-            case "ajolote":
+        Animales animal = Animales.valueOf(temaElegido);
+        switch (animal) {
+            case ajolote:
                 tv.setText(getString(R.string.historia_ajolote));
-            case "caballito":
+                toolBarLayout.setTitle(getString(R.string.ajolote));
+                toolBarLayout.setBackgroundResource(R.drawable.ajolote_historia);
+                break;
+            case caballito:
                 tv.setText(getString(R.string.historia_caballito));
-            case "langosta":
+                toolBarLayout.setTitle(getString(R.string.caballito_de_mar));
+                toolBarLayout.setBackgroundResource(R.drawable.caballito_historia);
+                break;
+            case langosta:
                 tv.setText(getString(R.string.historia_langosta));
-            case "lobo":
+                toolBarLayout.setTitle(getString(R.string.langosta));
+                toolBarLayout.setBackgroundResource(R.drawable.langosta_historia);
+                break;
+            case lobo:
                 tv.setText(getString(R.string.historia_lobo));
-            case "coral":
+                toolBarLayout.setTitle(getString(R.string.lobo_marino));
+                toolBarLayout.setBackgroundResource(R.drawable.lobomarino_historia);
+                break;
+            case coral:
                 tv.setText(getString(R.string.historia_coral));
-            case "anemona":
+                toolBarLayout.setTitle(getString(R.string.coral));
+                toolBarLayout.setBackgroundResource(R.drawable.coral_historia);
+                break;
+            case anemona:
                 tv.setText(getString(R.string.historia_anemona));
-            case "pez":
+                toolBarLayout.setTitle(getString(R.string.anemona_de_mar));
+                toolBarLayout.setBackgroundResource(R.drawable.anemona_historia);
+                break;
+            case pez:
                 tv.setText(getString(R.string.historia_pez));
-            case "foca":
+                toolBarLayout.setTitle(getString(R.string.pez_globo));
+                toolBarLayout.setBackgroundResource(R.drawable.pezglobo_historia);
+                break;
+            case foca:
+                toolBarLayout.setTitle(getString(R.string.foca));
                 tv.setText(getString(R.string.historia_foca));
-            case "tiburon":
+                toolBarLayout.setBackgroundResource(R.drawable.foca_historia);
+                break;
+            case tiburon:
                 tv.setText(getString(R.string.historia_tiburon));
-            case "tortuga":
+                toolBarLayout.setTitle(getString(R.string.tiburon_anguila));
+                toolBarLayout.setBackgroundResource(R.drawable.tiburon_historia);
+                break;
+            case tortuga:
                 tv.setText(getString(R.string.historia_tortuga));
-            case "ballena":
+                toolBarLayout.setTitle(getString(R.string.tortuga_marina));
+                toolBarLayout.setBackgroundResource(R.drawable.tortuga_historia);
+                break;
+            case ballena:
                 tv.setText(getString(R.string.historia_ballena));
-            case "narval":
+                toolBarLayout.setTitle(getString(R.string.ballena_azul));
+                toolBarLayout.setBackgroundResource(R.drawable.ballena_historia);
+                break;
+            case narval:
                 tv.setText(getString(R.string.historia_narval));
-            default:
-                tv.setText(getString(R.string.historia_narval));
+                toolBarLayout.setTitle(getString(R.string.narval));
+                toolBarLayout.setBackgroundResource(R.drawable.narval_historia);
+                break;
         }
-
- */
-        if(temaElegido.equals("tortuga")){
-            tv.setText(getString(R.string.historia_tortuga));
-            toolBarLayout.setTitle(getString(R.string.tortuga_marina));
-            toolBarLayout.setBackgroundResource(R.drawable.tortuga_historia);
-        }else if(temaElegido.equals("ajolote")){
-            tv.setText(getString(R.string.historia_ajolote));
-            toolBarLayout.setTitle(getString(R.string.ajolote));
-            toolBarLayout.setBackgroundResource(R.drawable.ajolote_historia);
-        }else if(temaElegido.equals("caballito")){
-            tv.setText(getString(R.string.historia_caballito));
-            toolBarLayout.setTitle(getString(R.string.caballito_de_mar));
-            toolBarLayout.setBackgroundResource(R.drawable.caballito_historia);
-        }else if(temaElegido.equals("langosta")){
-            tv.setText(getString(R.string.historia_langosta));
-            toolBarLayout.setTitle(getString(R.string.langosta));
-            toolBarLayout.setBackgroundResource(R.drawable.langosta_historia);
-        }else if(temaElegido.equals("coral")){
-            tv.setText(getString(R.string.historia_coral));
-            toolBarLayout.setTitle(getString(R.string.coral));
-            toolBarLayout.setBackgroundResource(R.drawable.coral_historia);
-        }else if(temaElegido.equals("lobo")){
-            tv.setText(getString(R.string.historia_lobo));
-            toolBarLayout.setTitle(getString(R.string.lobo_marino));
-            toolBarLayout.setBackgroundResource(R.drawable.lobomarino_historia);
-        }else if(temaElegido.equals("tiburon")){
-            tv.setText(getString(R.string.historia_tiburon));
-            toolBarLayout.setTitle(getString(R.string.tiburon_anguila));
-            toolBarLayout.setBackgroundResource(R.drawable.tiburon_historia);
-        }else if(temaElegido.equals("anemona")){
-            tv.setText(getString(R.string.historia_anemona));
-            toolBarLayout.setTitle(getString(R.string.anemona_de_mar));
-            toolBarLayout.setBackgroundResource(R.drawable.anemona_historia);
-        }else if(temaElegido.equals("narval")){
-            tv.setText(getString(R.string.historia_narval));
-            toolBarLayout.setTitle(getString(R.string.narval));
-            toolBarLayout.setBackgroundResource(R.drawable.narval_historia);
-        }else if(temaElegido.equals("ballena")){
-            tv.setText(getString(R.string.historia_ballena));
-            toolBarLayout.setTitle(getString(R.string.ballena_azul));
-            toolBarLayout.setBackgroundResource(R.drawable.ballena_historia);
-        }else if(temaElegido.equals("pez")){
-            tv.setText(getString(R.string.historia_pez));
-            toolBarLayout.setTitle(getString(R.string.pez_globo));
-            toolBarLayout.setBackgroundResource(R.drawable.pezglobo_historia);
-        }else if(temaElegido.equals("foca")){
-            toolBarLayout.setTitle(getString(R.string.foca));
-            tv.setText(getString(R.string.historia_foca));
-            toolBarLayout.setBackgroundResource(R.drawable.foca_historia);
-        }else{
-            tv.setText(getString(R.string.historia_ajolote));
-        }
-
         FloatingActionButton fab = binding.fab;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
